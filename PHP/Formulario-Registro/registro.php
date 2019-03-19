9f4819fd8e7ef570c7498bcd94bf7638a52b119e
Alejandra Camacho <camacho.ale94@gmail.com>
2019-03-19 13:31:46 -0600

    Ajuste de json_encode para envio de respuestas

--- PHP/Formulario-Registro/registro.php
+++ PHP/Formulario-Registro/registro.php
@@ -9,19 +9,25 @@
 	$passwd 	= trim(isset($_POST['contrasena']) 	? $_POST['contrasena'] 	: "");
 	$aCode 		= trim(isset($_POST['codigo']) 		? $_POST['codigo'] 		: "");
 
-	// Formato MD5 para password
-	$passwd = md5($passwd);
-
-	// Query
-	$sql = "INSERT INTO usuario (firstName, lastName, email, passwd, accessCode) VALUES ('$nombre', '$apellido', '$email', '$passwd', '$aCode')";
-	$link->query($sql);
-
-	# No funciona, arreglar
 	if (strlen($nombre) > 3) {
-		if (sterlen($apellido) > 3) {
+		if (strlen($apellido) > 3) {
 			if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
-				if ($passwd >= 8) {
-					# aquí debería ir la query
+				if (strlen($passwd) >= 8) {
+					if ($aCode == "qwerty123") {
+						// $passwd = md5($passwd);
+						$sql = "INSERT INTO usuario (firstName, lastName, email, passwd, accessCode) 
+								VALUES ('$nombre', '$apellido', '$email', '$passwd', '$aCode')";
+						$link->query($sql);
+						echo json_encode([
+							"status" => "OK",
+							"message" => "Su registro ha sido completado."
+						]);
+					} else {
+						echo json_encode([
+							"status" => "INVALID_CODE",
+							"message" => "El codigo de acceso no es valido."
+						]);
+					}
 				} else {
 					echo json_encode([
 						"status" => "INVALID_PASSWORD",
@@ -37,14 +43,45 @@
 		} else {
 			echo json_encode([
 				"status" => "INVALID_LASTNAME",
-				"message" => "Formato del apellido es inválido."
+				"message" => "El apellido es inválido."
 			]);
 		}
 	} else {
 		echo json_encode([
 			"status" => "INVALID_NAME",
-			"message" => "Formato del nombre es inválido."
+			"message" => "El nombre es inválido."
 		]);
 	}
+
+	# No funciona, arreglar
+	// if (strlen($nombre) > 3) {
+	// 	if (sterlen($apellido) > 3) {
+	// 		if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+	// 			if ($passwd >= 8) {
+	// 				# aquí debería ir la query
+	// 			} else {
+	// 				echo json_encode([
+	// 					"status" => "INVALID_PASSWORD",
+	// 					"message" => "La contraseña debe ser mayor a 8 caracteres."
+	// 				]);
+	// 			}
+	// 		} else {
+	// 			echo json_encode([
+	// 				"status" => "INVALID_EMAIL",
+	// 				"message" => "Formato del correo es inválido."
+	// 			]);
+	// 		}
+	// 	} else {
+	// 		echo json_encode([
+	// 			"status" => "INVALID_LASTNAME",
+	// 			"message" => "Formato del apellido es inválido."
+	// 		]);
+	// 	}
+	// } else {
+	// 	echo json_encode([
+	// 		"status" => "INVALID_NAME",
+	// 		"message" => "Formato del nombre es inválido."
+	// 	]);
+	// }
 
 ?>
